bfcfbdd0448806a302235951a06c9f5511d25193
Jinhyeok Yang <yangjh05@kaist.ac.kr>
2024-07-19 22:14:50 +0900

    Login Page Add-on 2

--- src/App.js
+++ src/App.js
@@ -1,117 +1,6 @@
-// import './App.css';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Nav from './component/Nav';
-// import Main from './component/Main';
-// import PlayerPage from './component/swissPages/PlayerPage';
-// import AddPlayerPage from './component/swissPages/AddPlayer';
-// import PlayerInfoPage from './component/swissPages/PlayerInfoPage';
-// import TournamentPage from './component/swissPages/TournamentPage';
-// import NoticePage from './component/swissPages/NoticePage';
-// import RoundPage from './component/swissPages/RoundPage';
-// import RoundInfoPage from './component/swissPages/RoundInfoPage';
-// import { PlayerProvider } from './component/swissPages/PlayerInfo';
-// import { RoundProvider } from './component/swissPages/RoundInfo';
-
-// const StartPage = () => {
-//   return (
-//     <div>
-//       <Nav />
-//       <Main />
-//     </div>
-//   );
-// }
-
-// const PlayerRender = () => {
-//   return (
-//     <div>
-//       <Nav />
-//       <Main />
-//       <PlayerPage />
-//     </div>
-//   );
-// }
-
-// const AddPlayerRender = () => {
-//   return (
-//     <div>
-//       <AddPlayerPage />
-//     </div>
-//   );
-// }
-
-// const PlayerInfoRender = ({id}) => {
-//   return (
-//     <div>
-//       <PlayerInfoPage />
-//     </div>
-//   );
-// }
-
-// const NoticeRender = () => {
-//   return (
-//     <div>
-//       <Nav />
-//       <Main />
-//       <NoticePage />
-//     </div>
-//   );
-// }
-
-// const TournamentRender = () => {
-//   return (
-//     <div>
-//       <Nav />
-//       <Main />
-//       <TournamentPage />
-//     </div>
-//   );
-// }
-
-// const RoundRender = () => {
-//   return (
-//     <div>
-//       <Nav />
-//       <Main />
-//       <RoundPage />
-//     </div>
-//   );
-// }
-
-// const RoundInfoRender = ({numS}) => {
-//   return (
-//     <div>
-//       <RoundInfoPage />
-//     </div>
-//   );
-// }
-
-// function App() {
-//   return (
-//     <PlayerProvider>
-//       <RoundProvider>
-//         <BrowserRouter>
-//           <div className='App'>
-//             <Routes>
-//               <Route path='/' element={<StartPage />}></Route>
-//               <Route path='/player' element={<PlayerRender />}></Route>
-//               <Route path='/player/addplayer' element={<AddPlayerRender />}></Route>
-//               <Route path='/player/info/:id' element={<PlayerInfoRender />}></Route>
-//               <Route path='/round' element={<RoundRender />}></Route>
-//               <Route path='/round/:numS' element={<RoundInfoRender />}></Route>
-//               <Route path='/tournament' element={<TournamentRender />}></Route>
-//               <Route path='/notice' element={<NoticeRender />}></Route>
-//             </Routes>
-//           </div>
-//         </BrowserRouter>
-//       </RoundProvider>
-//     </PlayerProvider>
-//   );
-// }
-
-// export default App;
-
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Nav from './component/Nav';
 import Main from './component/Main';
 import PlayerPage from './component/swissPages/PlayerPage';
@@ -123,6 +12,8 @@ import RoundPage from './component/swissPages/RoundPage';
 import RoundInfoPage from './component/swissPages/RoundInfoPage';
 import { PlayerProvider } from './component/swissPages/PlayerInfo';
 import { RoundProvider } from './component/swissPages/RoundInfo';
+import LoadingPage from "./component/LoadingPage";
+import LoginPage from "./component/LoginPage";
 
 const StartPage = () => {
   return (
@@ -142,13 +33,11 @@ const PlayerRender = () => {
   );
 }
 
-const AddPlayerRender = () => {
-  return (
-    <div>
-      <AddPlayerPage />
-    </div>
-  );
-}
+const AddPlayerRender = () => (
+  <div>
+    <AddPlayerPage />
+  </div>
+);
 
 const PlayerInfoRender = ({ id }) => {
   return (
@@ -194,20 +83,39 @@ const RoundInfoRender = ({ numS }) => {
 }
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLogin = () => {
+    setIsAuthenticated(true);
+  };
+
+  const handleLoadingEnd = () => {
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
+    return <LoadingPage onAnimationEnd={handleLoadingEnd} />;
+  }
+
+  if (!isAuthenticated) {
+    return <LoginPage onLogin={handleLogin} />;
+  }
+
   return (
     <PlayerProvider>
       <RoundProvider>
         <BrowserRouter>
-          <div className='App'>
+          <div className="App">
             <Routes>
-              <Route path='/' element={<StartPage />}></Route>
-              <Route path='/player' element={<PlayerRender />}></Route>
-              <Route path='/player/addplayer' element={<AddPlayerRender />}></Route>
-              <Route path='/player/info/:id' element={<PlayerInfoRender />}></Route>
-              <Route path='/round' element={<RoundRender />}></Route>
-              <Route path='/round/:numS' element={<RoundInfoRender />}></Route>
-              <Route path='/tournament' element={<TournamentRender />}></Route>
-              <Route path='/notice' element={<NoticeRender />}></Route>
+              <Route path="/" element={<StartPage />} />
+              <Route path="/player" element={<PlayerRender />} />
+              <Route path="/player/addplayer" element={<AddPlayerRender />} />
+              <Route path="/player/info/:id" element={<PlayerInfoRender />} />
+              <Route path="/round" element={<RoundRender />} />
+              <Route path="/round/:numS" element={<RoundInfoRender />} />
+              <Route path="/tournament" element={<TournamentRender />} />
+              <Route path="/notice" element={<NoticeRender />} />
             </Routes>
           </div>
         </BrowserRouter>
