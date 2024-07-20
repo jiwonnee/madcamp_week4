ea93e65613468c3e917e7ef974053ae44c967cf9
sinjiwon <shinjw0619@gmail.com>
2024-07-20 20:03:40 +0900

    commit

---
+++ client/src/component/pages/userinfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserInfo = () => {
+  return (
+    <div>
+      <h1>기본 정보</h1>
+      <p>여기에 사용자 기본 정보를 표시합니다.</p>
+    </div>
+  );
+};
+
+export default UserInfo;
