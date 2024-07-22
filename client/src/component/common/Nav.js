6629ecfb6e1d1b95d8a310ddd13582d6a6a45fd0
Jinhyeok Yang <yangjh05@kaist.ac.kr>
2024-07-22 16:06:07 +0900

    Button Design

--- client/src/component/common/Nav.js
+++ client/src/component/common/Nav.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../../assets/styles/css/Nav.css';
-import Logo from '../../assets/images/Khartes_Logo_Mini.png'; // 로고 이미지 임포트
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../assets/styles/css/Nav.css";
+import Logo from "../../assets/images/Khartes_Logo_Mini.png"; // 로고 이미지 임포트
 
-const Nav = ({user}) => {
+const Nav = ({ user }) => {
   return (
     <div className="nav-container">
       <div className="nav-left">
-        <Link className='icon' to={'/'}><img className="logoImg" src={Logo} alt='Logo' /></Link>
-        <Link className='app-title' to={'/'}>All-Rounder</Link>
+        <Link className="icon" to={"/"}>
+          <img className="logoImg" src={Logo} alt="Logo" />
+        </Link>
+        <Link className="app-title" to={"/"}>
+          All-Rounder
+        </Link>
       </div>
       <div className="nav-right">
         <div className="user-info">
-          <div className="user-circle">
-            <span className="user-name">{user.following_userid}</span>
+          <div className="logout">
+            <button className="btn btn-2">Logout</button>
           </div>
           <div className="dropdown">
-            <button className="dropdown-btn">▼</button>
+            <button className="btn dropdown-btn">
+              <svg>
+                <rect x="0" y="0" fill="none" width="100%" height="100%" />
+              </svg>
+              {user.following_userid}
+            </button>
             <div className="dropdown-content">
               <Link to="/userinfo">기본 정보</Link>
               <Link to="/joininfo">참여 정보</Link>
