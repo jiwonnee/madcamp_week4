88daf64b101817c339b6703bb2853b4db94b187a
sinjiwon <shinjw0619@gmail.com>
2024-07-22 19:41:06 +0900

    create page

--- client/src/component/pages/joininfo.js
+++ client/src/component/pages/joininfo.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Nav from '../common/Nav'; 
+import '../../assets/styles/css/JoinInfo.css'; 
 
-const JoinInfo = () => {
+const JoinInfo = ({ user }) => {
   return (
     <div>
-      <h1>참여 정보</h1>
-      <p>여기에 사용자 참여 정보를 표시합니다.</p>
+      <Nav user={user} />
+      <div className="join-info-container">
+        <h1>참여 정보</h1>
+        <p>여기에 사용자 참여 정보를 표시합니다.</p>
+      </div>
     </div>
   );
 };
