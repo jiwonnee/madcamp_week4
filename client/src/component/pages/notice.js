b0f37cdccc9603a5a4b194ef554f0ecf72f2a057
sinjiwon <shinjw0619@gmail.com>
2024-07-23 21:14:13 +0900

    main page ui

--- client/src/component/pages/notice.js
+++ client/src/component/pages/notice.js
@@ -1,14 +1,31 @@
+// Notice.js
 import React from 'react';
 import Nav from '../common/Nav';
 import '../../assets/styles/css/Notice.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBullhorn, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
 
 const Notice = ({ user }) => {
+  const notices = [
+    "All-Rounder의 회원이 되신 것을 축하드립니다!",
+    "원하는 이벤트에 참가하세요. 없으면 직접 이벤트를 추가해보세요!",
+  ];
+
   return (
     <div>
       <Nav user={user} />
+      <h1 className="title">공지사항</h1>
       <div className="notice-container">
-        <h1 className="title">공지사항</h1>
-        <p>여기에 공지사항 내용을 표시합니다.</p>
+        <div className="notice-header">
+          <FontAwesomeIcon icon={faBullhorn} /> <span className="notice-content">공지사항을 확인해 주세요.</span>
+        </div>
+        <ul className="notice-list">
+          {notices.map((notice, index) => (
+            <li key={index} className="notice-item">
+              <FontAwesomeIcon icon={faCheckSquare} className="notice-icon" /> {notice}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
