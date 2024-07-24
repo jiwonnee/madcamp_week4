b88ada7bfc3e4a0c66ddd1e8ecbadb5aa1a276f3
sinjiwon <shinjw0619@gmail.com>
2024-07-24 21:36:58 +0900

    UserInfo get info

--- client/src/component/pages/userinfo.js
+++ client/src/component/pages/userinfo.js
@@ -5,33 +5,49 @@ import '../../assets/styles/css/UserInfo.css';
 const UserInfo = ({ user }) => {
   const [userInfo, setUserInfo] = useState({});
   const [previousGames, setPreviousGames] = useState([]);
+  const token = localStorage.getItem("token") || sessionStorage.getItem("token");
 
   useEffect(() => {
-    // 사용자 정보를 가져오는 예시
-    fetch(`http://localhost:3001/api/userinfo/${user.id}`)
+    // 사용자 정보를 가져옴ㅌ
+    fetch(`http://localhost:3001/api/userinfo/${user.id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
       .then(response => response.json())
       .then(data => setUserInfo(data))
       .catch(error => console.error('Error fetching user info:', error));
 
     // 이전 경기 정보를 가져오는 예시
-    fetch(`http://localhost:3001/api/previousgames/${user.id}`)
+    fetch(`http://localhost:3001/api/previousgames/${user.id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
       .then(response => response.json())
       .then(data => setPreviousGames(data))
       .catch(error => console.error('Error fetching previous games:', error));
-  }, [user.id]);
+  }, [user.id, token]);
 
   return (
     <div>
       <Nav user={user} />
+      <h1 className="title">사용자 정보</h1>
       <div className="user-info-container">
-        <h1 className="title">사용자 정보</h1>
+        <h2 className="subtitle">기본 정보</h2>
         <hr />
         <div className="user-details">
-          <p>아이디: {userInfo.username}</p>
-          <p>패스워드: {userInfo.password}</p>
-          <p>이메일: {userInfo.email}</p>
+          {userInfo ? (
+            <>
+              <p>아이디: {user.following_userid}</p>
+              <p>패스워드: {user.following_password}</p>
+              <p>이메일: {user.email}</p>
+            </>
+          ) : (
+            <p>Loading...</p>
+          )}
         </div>
-        <h2 className="subtitle">이전 경기</h2>
+        <h2 className="subtitle_2">이전 경기</h2>
         <hr />
         <div className="previous-games">
           {previousGames.length > 0 ? (
